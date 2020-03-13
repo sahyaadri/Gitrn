e39dbddde1ffbb87d6fed07a49c2a72966a9751a
Aniruddha Patil (170384) <anirpati@amdocs.com>
2020-03-13 23:00:15 +0530

    New Commit

--- src/main/java/com/rdj/config/AppConfig.java
+++ src/main/java/com/rdj/config/AppConfig.java
@@ -7,6 +7,7 @@ public class AppConfig
         System.out.println("Line before ALL");
         System.out.println("Git Versioning");
         System.out.println("This is second line");
+        System.out.println("Middle line");
         System.out.println("This is third line");
         System.out.println("This is fourth line");
         System.out.println("This is fifth line");

0495ba214841ff18e7a6d83e41bd8e75508ab43e
Aniruddha Patil (170384) <anirpati@amdocs.com>
2020-03-13 22:01:59 +0530

    Fourth Commit

--- src/main/java/com/rdj/config/AppConfig.java
+++ src/main/java/com/rdj/config/AppConfig.java
@@ -4,6 +4,7 @@ package com.rdj.config;
 public class AppConfig
 {
     public static void main(String[] args) {
+        System.out.println("Line before ALL");
         System.out.println("Git Versioning");
         System.out.println("This is second line");
         System.out.println("This is third line");

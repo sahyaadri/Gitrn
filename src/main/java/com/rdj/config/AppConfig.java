2dddb2ab36088b461ff16707def636643696ca2e
Aniruddha Patil (170384) <anirpati@amdocs.com>
2020-03-13 21:30:54 +0530

    File Committed Second Times

--- src/main/java/com/rdj/config/AppConfig.java
+++ src/main/java/com/rdj/config/AppConfig.java
@@ -5,5 +5,7 @@ public class AppConfig
 {
     public static void main(String[] args) {
         System.out.println("Git Versioning");
+        System.out.println("This is second line");
+        System.out.println("This is third line");
     }
 }
